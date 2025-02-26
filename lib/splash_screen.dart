1a721fc5f2f784d2a0832da6f8f5611f226c46cc
AshenFerna29 <ashen2742@gmail.com>
2025-02-26 13:30:53 +0530

    Correct navigation to login page

--- lib/splash_screen.dart
+++ lib/splash_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'dart:async';
-import 'home_screen.dart';
+import 'login_screen.dart'; // Import LoginScreen
 
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
@@ -15,7 +15,7 @@ class _SplashScreenState extends State<SplashScreen> {
     super.initState();
     Timer(const Duration(seconds: 3), () {
       Navigator.of(context).pushReplacement(
-        MaterialPageRoute(builder: (context) => const HomeScreen()),
+        MaterialPageRoute(builder: (context) => const LoginScreen()), // Navigate to LoginScreen
       );
     });
   }
