321855ab1f66bf13b84654d95fd770a090920715
Kaveesha-2003 <kaveeshasulakshana33@gmail.com>
2025-03-10 01:02:24 +0530

    register page comfirmation fixed

--- lib/register_screen.dart
+++ lib/register_screen.dart
@@ -1,8 +1,46 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-class RegisterScreen extends StatelessWidget {
+class RegisterScreen extends StatefulWidget {
   const RegisterScreen({super.key});
+
+  @override
+  _RegisterScreenState createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final _formKey = GlobalKey<FormState>();
+
+  final TextEditingController firstNameController = TextEditingController();
+  final TextEditingController lastNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController mobileController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+
+  bool isChecked = false;
+  bool showErrorMessage = false;
+
+  void _validateAndSubmit() {
+    if (_formKey.currentState!.validate()) {
+      // Proceed with registration
+      Navigator.pop(context);
+    } else {
+      setState(() {
+        showErrorMessage = true;
+      });
+    }
+  }
+
+  bool _isFormFilled() {
+    return firstNameController.text.isNotEmpty &&
+        lastNameController.text.isNotEmpty &&
+        emailController.text.isNotEmpty &&
+        mobileController.text.isNotEmpty &&
+        passwordController.text.isNotEmpty &&
+        confirmPasswordController.text.isNotEmpty;
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,103 +49,128 @@ class RegisterScreen extends StatelessWidget {
         decoration: const BoxDecoration(
           gradient: RadialGradient(
             radius: 1.2,
-            colors: [
-              Color(0xFF011D47),
-              Color(0xFF00050B),
-              Color(0xFF00060E),
-            ],
+            colors: [Color(0xFF011D47), Color(0xFF00050B), Color(0xFF00060E)],
             stops: [0.0, 1.0, 1.0],
           ),
         ),
         child: Center(
           child: SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                const SizedBox(height: 60),
-                Padding(
-                  padding: const EdgeInsets.only(left: 20),
-                  child: IconButton(
-                    onPressed: () => Navigator.pop(context),
-                    icon: const Icon(Icons.arrow_back, color: Colors.white),
-                  ),
-                ),
-                Center(
-                  child: Text(
-                    "NEW USER\nREGISTRATION",
-                    textAlign: TextAlign.center,
-                    style: GoogleFonts.poppins(
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
-                      color: Colors.white,
+            child: Form(
+              key: _formKey,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  const SizedBox(height: 60),
+                  Padding(
+                    padding: const EdgeInsets.only(left: 20),
+                    child: IconButton(
+                      onPressed: () => Navigator.pop(context),
+                      icon: const Icon(Icons.arrow_back, color: Colors.white),
                     ),
                   ),
-                ),
-                const SizedBox(height: 20),
-                Padding(
-                  padding: const EdgeInsets.symmetric(horizontal: 20),
-                  child: Container(
-                    padding: const EdgeInsets.all(20),
-                    decoration: BoxDecoration(
-                      color: Colors.white.withOpacity(0.07),
-                      borderRadius: BorderRadius.circular(19),
+                  Center(
+                    child: Text(
+                      "NEW USER\nREGISTRATION",
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.poppins(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                        color: Colors.white,
+                      ),
                     ),
-                    child: Column(
-                      children: [
-                        _buildLabel("Name"),
-                        _buildTextField("First Name"),
-                        const SizedBox(height: 15),
-                        _buildTextField("Last Name"),
-                        const SizedBox(height: 20),
-                        _buildLabel("Email"),
-                        _buildTextField("Email"),
-                        const SizedBox(height: 20),
-                        _buildLabel("Mobile Number"),
-                        _buildTextField("Mobile Number"),
-                        const SizedBox(height: 20),
-                        _buildLabel("Password"),
-                        _buildTextField("Password", isPassword: true),
-                        const SizedBox(height: 15),
-                        _buildTextField("Confirm Password", isPassword: true),
-                        const SizedBox(height: 10),
-                        Row(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Checkbox(
-                              value: false,
-                              onChanged: (value) {},
-                              activeColor: Colors.white,
-                            ),
-                            Expanded(
+                  ),
+                  const SizedBox(height: 20),
+                  Padding(
+                    padding: const EdgeInsets.symmetric(horizontal: 20),
+                    child: Container(
+                      padding: const EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                        color: Colors.white.withOpacity(0.07),
+                        borderRadius: BorderRadius.circular(19),
+                      ),
+                      child: Column(
+                        children: [
+                          _buildLabel("Name"),
+                          _buildTextField("First Name", firstNameController),
+                          const SizedBox(height: 15),
+                          _buildTextField("Last Name", lastNameController),
+                          const SizedBox(height: 20),
+                          _buildLabel("Email"),
+                          _buildTextField("Email", emailController),
+                          const SizedBox(height: 20),
+                          _buildLabel("Mobile Number"),
+                          _buildTextField("Mobile Number", mobileController),
+                          const SizedBox(height: 20),
+                          _buildLabel("Password"),
+                          _buildTextField(
+                            "Password",
+                            passwordController,
+                            isPassword: true,
+                          ),
+                          const SizedBox(height: 15),
+                          _buildTextField(
+                            "Confirm Password",
+                            confirmPasswordController,
+                            isPassword: true,
+                          ),
+                          const SizedBox(height: 10),
+                          Row(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Checkbox(
+                                value: isChecked,
+                                onChanged: (value) {
+                                  setState(() {
+                                    isChecked = value!;
+                                  });
+                                },
+                                activeColor: Colors.white,
+                              ),
+                              Expanded(
+                                child: Text(
+                                  "Allow location access for personalized updates. Manage preferences anytime in settings. View our Privacy Policy for details.",
+                                  style: GoogleFonts.poppins(
+                                    color: Colors.white,
+                                    fontSize: 11,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          ),
+                          if (showErrorMessage)
+                            Padding(
+                              padding: const EdgeInsets.only(top: 10),
                               child: Text(
-                                "Allow location access for personalized updates. Manage preferences anytime in settings. View our Privacy Policy for details.",
+                                "Fill these fields",
                                 style: GoogleFonts.poppins(
-                                  color: Colors.white,
-                                  fontSize: 11,
+                                  color: Colors.red,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.bold,
                                 ),
                               ),
                             ),
-                          ],
-                        ),
-                        const SizedBox(height: 10),
-                        ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: const Color(0xFF3B43D6),
-                            minimumSize: const Size(double.infinity, 50),
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(11),
+                          const SizedBox(height: 10),
+                          ElevatedButton(
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: const Color(0xFF3B43D6),
+                              minimumSize: const Size(double.infinity, 50),
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(11),
+                              ),
+                            ),
+                            onPressed:
+                                _isFormFilled() ? _validateAndSubmit : null,
+                            child: const Text(
+                              "REGISTER",
+                              style: TextStyle(color: Colors.white),
                             ),
                           ),
-                          onPressed: () {
-                            Navigator.pop(context);
-                          },
-                          child: const Text("REGISTER", style: TextStyle(color: Colors.white)),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
           ),
         ),
@@ -115,8 +178,13 @@ class RegisterScreen extends StatelessWidget {
     );
   }
 
-  Widget _buildTextField(String hint, {bool isPassword = false}) {
-    return TextField(
+  Widget _buildTextField(
+    String hint,
+    TextEditingController controller, {
+    bool isPassword = false,
+  }) {
+    return TextFormField(
+      controller: controller,
       obscureText: isPassword,
       style: const TextStyle(color: Colors.white),
       decoration: InputDecoration(
@@ -129,6 +197,17 @@ class RegisterScreen extends StatelessWidget {
           borderSide: const BorderSide(color: Colors.white60),
         ),
       ),
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "This field is required";
+        }
+        return null;
+      },
+      onChanged: (value) {
+        setState(() {
+          showErrorMessage = false;
+        });
+      },
     );
   }
 
